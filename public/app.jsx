76f5b1a1334e7ab283bfd0eebf8175624c207f31
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-19 11:54:46 -0300

    Wires up nested components.

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,47 @@
 var GreeterMessage = React.createClass({
   render: function () {
+
+    var name = this.props.name;
+    var message = this.props.message;
+    var text = this.props.text; // readonly
+
     return (
       <div>
-        <h1>Some H1</h1>
-        <p>Some P</p>
+        <h1>Hello {name}</h1>
+        <p>{message}</p>
+        <p>{text}</p>
       </div>
     );
   }
 });
 
 var GreeterForm = React.createClass({
+
+  onFormSubmit: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      this.props.onNewMessage(message);
+    }
+  },
+
   render: function () {
     return (
-      <form>
+      <form onSubmit={this.onFormSubmit}>
         <input type='text' ref='name'/>
         <button>Set Name</button>
+        <br/>
+        <input type='text' ref='message'/>
+        <button>Set Message</button>
       </form>
     )
   }
@@ -25,50 +52,42 @@ var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
       name: 'React',
-      message: 'Default message given by Component defaults'
+      message: 'Default message given by Component defaults',
+      text: "Sample text to be fetch by 'this.props'"
     }
   },
 
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     }
   },
 
-  onButtonClick: function (e) {
-    e.preventDefault();
+  handleNewName: function (name) {
+    this.setState({
+      name: name
+    });
+  },
 
-    var nameRef = this.refs.name;
-    var name = nameRef.value; // || this.props.name;
-    nameRef.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewMessage: function (message) {
+    this.setState({
+      message: message
+    });
   },
 
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
+    var text = this.props.text;
     //return React.createElement('h1', {id: 'asdf'}, 'Hello React.createElement')
 
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}</p>
-
-        <GreeterMessage/>
-
-        <form onSubmit={this.onButtonClick}>
-          {/* `ref` is a react referencer to the tag element. In this case, the
-              following input would be accessible by the `name` reference */}
-          <input type='text' ref='name'/>
-          <button>Set Name</button>
-        </form>
-
-        <GreeterForm/>
+        <GreeterMessage name={name} message={message} text={text}/>
+        <GreeterForm onNewName={this.handleNewName}
+                     onNewMessage={this.handleNewMessage}
+        />
       </div>
     );
   }
