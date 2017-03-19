d8c6758a9fb483392fc7846ba40a2fbaf8dc90dc
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-18 23:15:24 -0300

    Adds v1 -> hello world for react

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Express ta rolando na porta 3000'));
