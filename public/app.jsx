c2988898b28518cabcfc0a80e6f846bb98c158c0
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-19 10:03:25 -0300

    Adds state change to component

--- public/app.jsx
+++ public/app.jsx
@@ -7,15 +7,28 @@ var Greeter = React.createClass({
     }
   },
 
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
+
   onButtonClick: function (e) {
     e.preventDefault();
 
-    var name = this.refs.name.value;
-    alert(name);
+    var nameRef = this.refs.name;
+    var name = nameRef.value; // || this.props.name;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
   },
 
   render: function () {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
     //return React.createElement('h1', {id: 'asdf'}, 'Hello React.createElement')
 
