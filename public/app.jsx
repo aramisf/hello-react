2723450ee3fabb8c12fd06243d75c797e5992266
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-19 09:45:55 -0300

    Adds the first form

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,19 @@
 var Greeter = React.createClass({
+
   getDefaultProps: function () {
     return {
       name: 'React',
       message: 'Default message given by Component defaults'
     }
   },
+
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    alert(name);
+  },
+
   render: function () {
     var name = this.props.name;
     var message = this.props.message;
@@ -14,6 +23,13 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}!</h1>
         <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          {/* `ref` is a react referencer to the tag element. In this case, the
+              following input would be accessible by the `name` reference */}
+          <input type='text' ref='name'/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
