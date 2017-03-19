86d6158891fafaa227254ee45c84c11c86824498
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-19 09:00:19 -0300

    Translatin to English.

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Express ta rolando na porta 3000'));
+app.listen(3000, () => console.log('Express listening on port 3000'));
