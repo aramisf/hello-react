f261c3d5b771844d3035d4cf933e524ca33449d9
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-18 23:45:19 -0300

    Adds another way to show how React internals work.

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,16 @@
 var Greeter = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h1>Hello React!</h1>
-                <p>Paragraph rendered from inside the component :)</p>
-            </div>
-        );
-    }
+  render: function () {
+    //return React.createElement('h1', {id: 'asdf'}, 'Hello React.createElement')
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>Paragraph rendered from inside the component :)</p>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
-    <Greeter/>,
-    document.getElementById('app')
+  <Greeter/>,
+  document.getElementById('app')
 );
