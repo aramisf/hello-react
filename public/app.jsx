90d4863e8c87f9633e0a81e3b587754e22eb8df3
Aramis S. H. Fernandes <aramis.fernandes@gmail.com>
2017-03-19 09:25:24 -0300

    Adding default values to component.

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,28 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'Default message given by Component defaults'
+    }
+  },
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
     //return React.createElement('h1', {id: 'asdf'}, 'Hello React.createElement')
+
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>Paragraph rendered from inside the component :)</p>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Aramis';
+var message = "Message inserted via variable";
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={message}/>,
   document.getElementById('app')
 );
